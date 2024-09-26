e45a03d081acf67f4949eaebdf5a948ca0dbec12
Usama Sheikh <usamasheikhoffical786@gmail.com>
2024-09-26 21:13:22 +0500

    push assignment two component GIAIC class2

---
+++ app/components/hompage.tsx
@@ -0,0 +1,8 @@
+function Header() {
+    return(
+        <>
+        <h1>My second project Component</h1>
+        </>
+    )
+}
+export default Header
---
+++ app/page.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Header from "./components/hompage";
+import About from "./components/about";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <br />
+      <p>
+        <h4>Page content</h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+        facilis, dolorem incidunt sit aliquid eos est itaque repudiandae
+        deserunt quia et, velit cum magnam vel porro! Explicabo veniam pariatur,
+        hic rem, eligendi culpa itaque similique eos reiciendis molestiae
+        excepturi fugit. Itaque, fugiat eaque. Earum, est corporis voluptatibus
+        maiores iste necessitatibus atque assumenda reiciendis veniam nostrum
+        minima! Veniam atque voluptate explicabo, consectetur perferendis
+        maiores pariatur aperiam aliquid debitis modi quod minus voluptatem
+        facere voluptatibus tempore excepturi at sint alias fugiat cupiditate
+        neque! Maiores, quia error explicabo velit commodi, reiciendis nobis
+        perspiciatis eos fuga vitae sed earum. Odio provident ex consequatur
+        ullam tenetur quos aliquam rem facere impedit iure, error aut fugiat
+        tempora sapiente laborum accusantium sunt cupiditate natus placeat quod
+        sed consectetur. Ipsa assumenda amet esse error, maiores odio quibusdam
+        eum accusamus, asperiores vitae aperiam ad commodi iusto praesentium,
+        placeat autem rerum. Nostrum tenetur, atque explicabo veritatis eos
+        voluptatibus aspernatur laudantium. Ea, vero? Eaque nam, voluptatibus
+        doloribus reprehenderit temporibus eos laborum illum, quod deserunt vel
+        dolores ipsa dolorum tenetur minima? Illum eos, molestias blanditiis rem
+        doloribus autem itaque deleniti mollitia magnam est labore accusantium
+        et. Eligendi iste provident laborum! Voluptatem deleniti quis
+        consequatur velit. A vel reiciendis dicta. Saepe nisi pariatur magni
+        consequatur nemo qui mollitia, sed dicta. Porro cupiditate temporibus,
+        culpa, quibusdam voluptate magni voluptatum obcaecati, atque magnam non
+        ea velit? Atque, voluptas! Fugit perspiciatis asperiores quisquam illum
+        repellendus eum unde, dignissimos aliquam voluptatem adipisci nulla
+        sequi hic placeat inventore corporis, provident ut at dolorum rerum quia
+        tenetur! Ab ad dolorum perferendis voluptate obcaecati tenetur
+        praesentium modi earum aut? Eligendi dignissimos ipsam ducimus dolores
+        tempora a assumenda cum? Eius eveniet vel sed consequatur quidem fugit
+        esse, ratione aut quibusdam ea. Repudiandae modi vitae quidem, esse
+        consectetur molestias omnis non maiores maxime similique repellendus
+        tenetur natus error, iure magni asperiores ipsum. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Itaque ipsam aliquam ut eaque
+        molestias iusto consequuntur ex, cum sit et facilis, quia quaerat
+        sapiente voluptatum. Asperiores minus velit voluptatibus dolore?Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Laudantium quidem
+        quasi eum, dolorem aspernatur tempore blanditiis! Commodi incidunt
+        inventore non ut aspernatur dignissimos, pariatur, reiciendis dolor,
+        placeat minus provident iusto.
+      </p>
+      <br />
+      <About />
+    </div>
+  );
+}
